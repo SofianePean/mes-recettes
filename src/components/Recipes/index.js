c4b464aadc1f26ea27624b9ec496846cf34847f8
Sofiane <pean.sofiane@gmail.com>
2021-01-21 15:08:02 +0100

    mise en place du projet

---
+++ src/components/Recipes/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View} from 'react-native';
+
+// Import components
+import Recipe from './recipe'
+
+const Recipes = () => {
+    return(
+        <View>
+            <Recipe />
+        </View>
+    )
+}
+
+export default Recipes;
---
+++ src/components/Recipes/recipe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Recipe = () => {
+    return(
+        <View>
+            <Text>Recipe</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default Recipe;
